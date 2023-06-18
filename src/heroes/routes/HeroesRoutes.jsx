4491c409fda40c72892007ee70088126789dff43
josemek098dev <josemek098dev@gmail.com>
2023-06-18 19:59:26 -0300

    first commit

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,26 @@
+import { Navbar } from "../../ui"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { MarvelPage } from "../pages/MarvelPage"
+import { DCPage } from "../pages/DCPage"
+import { SearchPage } from "../pages/SearchPage"
+import { HeroPage } from "../pages/HeroPage"
+
+export const HeroesRoutes = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="/marvel" element={<MarvelPage />} />
+                    <Route path="/dc" element={<DCPage />} />
+
+                    <Route path="/search" element={<SearchPage />} />
+                
+                    <Route path="hero/:id" element={<HeroPage />} />
+
+                    <Route path="/" element={<Navigate to="/marvel" />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
